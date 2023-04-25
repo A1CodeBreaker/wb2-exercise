9ee03bc96af9a56bac660da8d3c68d54f58db41d
A1CodeBreaker <129992490+A1CodeBreaker@users.noreply.github.com>
2023-04-25 14:22:51 -0400

    Update rule_of_72.js

--- rule_of_72.js
+++ rule_of_72.js
@@ -1,11 +1,14 @@
-//at a x % interest rate, your saving account will be worth y in z years
+//At a x % interest rate, your savings accoubt will be worth y in z years
 // var interest, principle, worth, years;
 
 //sample data
 var interest = 7;
 var principle = 10000;
 
+//calc
 var years = 72 / interest;
 var worth = principle * 2;
-//calc
-console.log("At a" + interest + "% interest rate, your savings account wil be worth $"+worth+"in"+ years.toFixed(1)+"years");
+
+//output
+console.log("At a " + interest + "% interest rate, your savings account will be worth $"+ worth.toFixed(2) +" in "+ years.toFixed(1) +" years");
+ 
